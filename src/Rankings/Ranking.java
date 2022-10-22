e5e012648c6015c0f7618b75e0816275c2ea949f
AlexDeAguiar <alexdeag@ucm.es>
2022-10-22 16:09:11 +0200

    Added a few rankings to test out

---
+++ src/Rankings/Ranking.java
@@ -0,0 +1,31 @@
+package Rankings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Ranking {
+	private static final int NumLabels = 13*13;
+	static double[][] matrix = new double[13][13];
+	
+	protected Ranking() {
+		initMatrix();
+	}
+	
+	protected abstract void initMatrix();
+	
+	public static List<Double> matrixToList(){
+		List<Double> list = new ArrayList<>();
+		
+		for(int i = 0; i < 13; i++) {
+			for(int j = 0; j < 13; j++) {
+				list.add(matrix[i][j]);
+			}
+		}
+		
+		return list;
+		
+	}
+	public double getNum(int i, int j) {
+		return matrix[i][j];
+	}
+}
